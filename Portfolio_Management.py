4fe10f7abe733b2e31d33a63a3d8a99637a25095
Mars <110466073+wcsmars@users.noreply.github.com>
2024-10-28 22:10:00 -0700

    #

--- Portfolio_Management.py
+++ Portfolio_Management.py
@@ -3,10 +3,11 @@ import numpy as np
 import datetime as dt
 import yfinance as yf
 import scipy.optimize as sc
+import plotly.graph_objects as go
 
 def getData(portfolio, start, end):
     stockData = yf.download(portfolio, start = start, end = end)
-    stockDatas = stockData['Close']
+    stockDatas = stockData['Close'].ffill()
     returns = stockDatas.pct_change().dropna()
     meanReturns = returns.mean()
     covarianceMatrix = returns.cov()
@@ -58,24 +59,115 @@ def minimizeVariance(meanReturns, covarianceMatrix):
 
 '''
 
+Efficient optimization
+
+'''
+
+
+def portfolioReturn(weights, meanReturns, covarianceMatrix):
+    return portfolioPerformance(weights, meanReturns, covarianceMatrix)[0]
+
+def efficient_optimization(meanReturns, covarianceMatrix, returnTarget):
+    num_of_assets = len(portfolio)
+    args = (meanReturns, covarianceMatrix)
+    bounds = tuple((0,1) for asset in range(num_of_assets))
+
+
+    constraints = ({'type': 'eq', 'fun': lambda x: portfolioReturn(x, meanReturns, covarianceMatrix) - returnTarget},
+                   {'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
+    
+
+    efficient_optimization_result = sc.minimize(portfolioVariance, num_of_assets * [1. / num_of_assets], args = args,
+                                                method = 'SLSQP', bounds = bounds, constraints = constraints)
+    
+    return efficient_optimization_result
+
+
+'''
+
 Output result function
 
 '''
 
-def sharpe_outputs(meanReturns, risk_free_rate, covarianceMatrix):
+# def sharpe_outputs(meanReturns, risk_free_rate, covarianceMatrix):
+#     maxSharpe = maxSR(meanReturns, covarianceMatrix, risk_free_rate)
+#     maxSharpe_Return, maxSharpe_STD = portfolioPerformance(maxSharpe['x'], meanReturns, covarianceMatrix)
+#     maxSharpe_Allocation = pd.DataFrame(maxSharpe['x'], index = meanReturns.index, columns = ['Weights'])
+#     maxSharpe_Allocation.Weights = [round(i * 100, 0) for i in maxSharpe_Allocation.Weights]
+#     return maxSharpe_Return, maxSharpe_STD, maxSharpe_Allocation
+
+# def minimumVariance_outputs(meanReturns, covarianceMatrix):
+#     minimumVariance = minimizeVariance(meanReturns, covarianceMatrix)
+#     minimumVariance_Return, minimumVariance_STD = portfolioPerformance(minimumVariance['x'], meanReturns, covarianceMatrix)
+#     minimumVariance_Allocation = pd.DataFrame(minimumVariance['x'], index = meanReturns.index, columns = ['Weights'])
+#     minimumVariance_Allocation.Weights = [round(i * 100, 0) for i in minimumVariance_Allocation.Weights]
+#     return minimumVariance_Return, minimumVariance_STD, minimumVariance_Allocation
+
+
+def results(meanReturns, covarianceMatrix, risk_free_rate):
     maxSharpe = maxSR(meanReturns, covarianceMatrix, risk_free_rate)
     maxSharpe_Return, maxSharpe_STD = portfolioPerformance(maxSharpe['x'], meanReturns, covarianceMatrix)
     maxSharpe_Allocation = pd.DataFrame(maxSharpe['x'], index = meanReturns.index, columns = ['Weights'])
-    maxSharpe_Allocation.Weights = [round(i * 100, 0) for i in maxSharpe_Allocation.Weights]
-    return maxSharpe_Return, maxSharpe_STD, maxSharpe_Allocation
+    
 
-def minimumVariance_outputs(meanReturns, covarianceMatrix):
     minimumVariance = minimizeVariance(meanReturns, covarianceMatrix)
     minimumVariance_Return, minimumVariance_STD = portfolioPerformance(minimumVariance['x'], meanReturns, covarianceMatrix)
     minimumVariance_Allocation = pd.DataFrame(minimumVariance['x'], index = meanReturns.index, columns = ['Weights'])
-    minimumVariance_Allocation.Weights = [round(i * 100, 0) for i in minimumVariance_Allocation.Weights]
-    return minimumVariance_Return, minimumVariance_STD, minimumVariance_Allocation
+    
 
+    efficient_list = []
+    targetReturns = np.linspace(minimumVariance_Return, maxSharpe_Return, 100)
+    for target in targetReturns:
+        efficient_list.append(efficient_optimization(meanReturns, covarianceMatrix, target)['fun']) # portfolio variance
+
+    maxSharpe_Allocation.Weights = [round(i * 100, 0) for i in maxSharpe_Allocation.Weights]
+    minimumVariance_Allocation.Weights = [round(i * 100, 0) for i in minimumVariance_Allocation.Weights]
+
+    return maxSharpe_Return, maxSharpe_STD, maxSharpe_Allocation, minimumVariance_Return, minimumVariance_STD, maxSharpe_Allocation, efficient_list, targetReturns
+    
+def efficient_frontier(meanReturns, covarianceMatrix):
+    maxSharpe_Return, maxSharpe_STD, maxSharpe_Allocation, minimumVariance_Return, minimumVariance_STD, maxSharpe_Allocation, efficient_list, targetReturns = results(meanReturns, covarianceMatrix, risk_free_rate)
+
+    maxSharpeRatio = go.Scatter(
+        name = 'Maximum Sharpe Ratio',
+        mode = 'markers',
+        x = [maxSharpe_STD],
+        y = [maxSharpe_Return],
+        marker = dict(color = 'blue', size = 13, line = dict(width = 3, color = 'black'))
+    )
+
+    minimumVariance = go.Scatter(
+        name = 'Minimum Variance',
+        mode = 'markers',
+        x = [minimumVariance_STD],
+        y = [minimumVariance_Return],
+        marker = dict(color = 'yellow', size = 13, line = dict(width = 3, color = 'black'))
+    )
+
+    EF_curve = go.Scatter(
+        name = 'Efficient Frontier',
+        mode = 'lines',
+        x = [ef_std for ef_std in efficient_list],
+        y = [target for target in targetReturns],
+        line = dict(color = 'black', width = 3, dash = 'dashdot')
+    )
+
+    data = [maxSharpeRatio, minimumVariance, EF_curve]
+
+    layout = go.Layout(
+        title = 'Portfolio Optimiziation with Efficient Frontier',
+        yaxis = dict(title = 'Annualized Return'),
+        xaxis = dict(title = 'Annualized Standard Deviation'),
+        showlegend = True,
+        legend = dict(
+            x = 0.75, y = 0, traceorder = 'normal', bgcolor = '#E2E2E2',
+            bordercolor = 'black', borderwidth = 2),
+        width = 800,
+        height = 800
+    )
+
+    fig = go.Figure(data = data, layout = layout)
+    return fig.show()
 
 '''
 
@@ -111,4 +203,10 @@ returns, standard_deviation = portfolioPerformance(weights, meanReturns, covaria
 
 # print(sharpe_outputs(meanReturns, risk_free_rate, covarianceMatrix))
 
-print(minimumVariance_outputs(meanReturns, covarianceMatrix))
+# print(minimumVariance_outputs(meanReturns, covarianceMatrix))
+
+# print(efficient_optimization(meanReturns, covarianceMatrix, 0.2)) # minimum target return to be 20%
+
+# print(results(meanReturns, covarianceMatrix, risk_free_rate))
+
+efficient_frontier(meanReturns, covarianceMatrix)
